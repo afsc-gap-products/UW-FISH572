4a53b84ef6b4377d52fc840638e720b7b2215bbc
Em Markowitz (NOAA) <emily.markowitz@noaa.gov>
2025-09-06 21:09:18 -0700

    fix here issue

--- content/functions.R
+++ content/functions.R
@@ -9,13 +9,13 @@ crs_out <- crs.out <- "EPSG:3338"
 
 # library(RCurl)
 write.table(x = readLines(con = "https://raw.githubusercontent.com/citation-style-language/styles/master/apa-no-ampersand.csl"),
-            file = here::here("content/references.csl"),
+            file = "./content/references.csl",
             row.names = FALSE,
             col.names = FALSE,
             quote = FALSE)
 
 write.table(x = readLines(con = "https://raw.githubusercontent.com/afsc-gap-products/citations/main/cite/bibliography.bib"),
-            file = here::here("content/references.bib"),
+            file = "./content/references.bib",
             row.names = FALSE,
             col.names = FALSE,
             quote = FALSE)
