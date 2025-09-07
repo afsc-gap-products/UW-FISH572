adc5358cdc8e1485839c37e2e1170af7ce5b5dd0
Em Markowitz (NOAA) <emily.markowitz@noaa.gov>
2025-09-06 19:25:04 -0700

    fixed csl issue

--- content/functions.R
+++ content/functions.R
@@ -1,3 +1,23 @@
 
 
 link_repo <- "https://github.com/afsc-gap-products/UW-FISH572"
+
+pretty_date <- format(Sys.Date(), "%B %d, %Y")
+crs_out <- crs.out <- "EPSG:3338"
+
+# Download citations -----------------------------------------------------------
+
+# library(RCurl)
+write.table(x = readLines(con = "https://raw.githubusercontent.com/citation-style-language/styles/master/apa-no-ampersand.csl"),
+            file = here::here("content/references.csl"),
+            row.names = FALSE,
+            col.names = FALSE,
+            quote = FALSE)
+
+write.table(x = readLines(con = "https://raw.githubusercontent.com/afsc-gap-products/citations/main/cite/bibliography.bib"),
+            file = here::here("content/references.bib"),
+            row.names = FALSE,
+            col.names = FALSE,
+            quote = FALSE)
+
+
